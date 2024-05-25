2934dd7295575594ff91f3524ef55b3ce48377af
kareem <kareem@kareem.lan>
2024-05-25 18:27:24 +0100

    #IA4

--- app/server.go
+++ app/server.go
@@ -10,9 +10,6 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -25,15 +22,22 @@ func main() {
 		os.Exit(1)
 	}
 
+	ReadFromConnection(conn)
+}
+
+func ReadFromConnection(conn net.Conn) {
+	const Http_Status_200 = "HTTP/1.1 200 OK\r\n\r\n"
+	const Http_Status_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
+
 	buff := make([]byte, 1024)
-	if _, err = conn.Read(buff); err != nil {
+	if _, err := conn.Read(buff); err != nil {
 		fmt.Println("Failed to read from connection")
 		os.Exit(1)
 	}
 
 	if strings.HasPrefix(string(buff), "GET / HTTP/1.1") {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		conn.Write([]byte(Http_Status_200))
 	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Write([]byte(Http_Status_404))
 	}
 }
