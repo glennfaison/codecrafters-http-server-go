8d5c809eb9a4fbd2f1101ea4884dfe0e35a71e7b
kareem <kareem@kareem.lan>
2024-05-27 01:00:58 +0100

    #CN2

--- app/server.go
+++ app/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 
 	// Uncomment this block to pass the first stage
@@ -16,49 +17,53 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer listener.Close()
 	fmt.Println("Listening...")
 
-	connectionChannel := make(chan net.Conn)
+	// connectionChannel := make(chan net.Conn)
 	for {
 		fmt.Println("before accepting connection")
-		conn, err := listener.Accept()
+		connection, err := listener.Accept()
 		fmt.Println("Connection accepted")
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			continue
 		}
-		fmt.Println("Sending connection to channel...")
-		connectionChannel <- conn
-		fmt.Println("Connection sent to channel...")
-		<-connectionChannel
-		go ReadFromConnection(connectionChannel)
+		handleConnection(connection)
 	}
 }
 
-func ReadFromConnection(connectionChannel chan net.Conn) {
-	fmt.Println("reading from connection...")
-	conn := <-connectionChannel
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
 	const Http_Status_200 = "HTTP/1.1 200 OK\r\n\r\n"
 	const Http_Status_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
+	const Http_Status_500 = "HTTP/1.1 500 Internal Server Error\r\n\r\n"
 
 	buff := make([]byte, 1024)
-	if _, err := conn.Read(buff); err != nil {
+	charactersRead, err := connection.Read(buff)
+	if err != nil {
 		fmt.Println("Failed to read from connection")
-		os.Exit(1)
+		connection.Write([]byte(Http_Status_500))
 	}
+	stringReceived := string(buff[:charactersRead])
+	println("received string", stringReceived)
 
 	pattern, err := regexp.Compile(`GET /echo/(\w+)`)
 	if err != nil {
 		fmt.Println("Failed to parse request")
 	}
-	strs := pattern.FindAllString(string(buff), -1)
-	for _, str := range strs {
-		println(str)
+
+	strs := pattern.FindStringSubmatch(stringReceived)
+	if len(strs) > 0 {
+		match := strs[1]
+		response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(match)) + "\r\n\r\n" + match
+		connection.Write([]byte(response))
+		return
 	}
 
 	if strings.HasPrefix(string(buff), "GET / HTTP/1.1") {
-		conn.Write([]byte(Http_Status_200))
+		connection.Write([]byte(Http_Status_200))
 	} else {
-		conn.Write([]byte(Http_Status_404))
+		connection.Write([]byte(Http_Status_404))
 	}
 }
